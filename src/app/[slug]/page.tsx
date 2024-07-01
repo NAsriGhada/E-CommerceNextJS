1849b599dea0dff160dfa0fc04692d2eb891b7d8
Ghada Nasri <nasrighada10@outlook.com>
2024-07-01 21:34:56 +0100

    first commit

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>slug page</div>
+  )
+}
+
+export default Page
---
+++ src/app/components/ProductList.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const ProductList = () => {
+  return (
+    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/26076624/pexels-photo-26076624/free-photo-of-beach-umbrella-and-chair-on-copacabana-beach-on-seashore-in-rio-de-janeiro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/25772466/pexels-photo-25772466/free-photo-of-a-grassy-hill-with-a-fence-and-a-fence.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl ring-1 ring-rose text-rose w-max p-2 px-4 text-xs hover:bg-rose hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default ProductList;
